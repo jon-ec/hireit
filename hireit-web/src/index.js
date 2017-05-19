824ce664b2c6ccaacbe4367d84117c5f97d9f11a
Jonathan Ellis-Craig <jonathan.craig@wipro.com>
2017-05-19 12:23:54 +0100

    added bootstrap css

--- hireit-web/src/index.js
+++ hireit-web/src/index.js
@@ -4,5 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
